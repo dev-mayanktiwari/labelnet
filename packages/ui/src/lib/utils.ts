019794dc33dffb37acd1ee7baf113f883e56f39d
Mayank <mayanktiwari1212.mt@gmail.com>
2025-05-21 23:00:13 +0530

    feat: Added UI components

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function truncateAddress(address: string, chars = 4): string {
+  if (!address) return "";
+  return `${address.substring(0, chars)}...${address.substring(address.length - chars)}`;
 }
