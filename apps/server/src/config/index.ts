21c7f10e88e75e1a37684c33b20007e8f7d6ed05
Mayank <mayanktiwari1212.mt@gmail.com>
2025-05-22 22:54:41 +0530

    feat: Updated config

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -1,13 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-type ConfigKeys = "PORT" | "NODE_ENV" | "JWT_SECRET" | "CLOUDINARY_API_SECRET";
+type ConfigKeys =
+  | "PORT"
+  | "NODE_ENV"
+  | "JWT_SECRET"
+  | "CLOUDINARY_API_SECRET"
+  | "SOLANA_RPC_URL"
+  | "ADMIN_PUBLIC_KEY"
+  | "ADMIN_PRIVATE_KEY";
 
 const _config: Record<ConfigKeys, string | undefined> = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
   JWT_SECRET: process.env.JWT_SECRET,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  SOLANA_RPC_URL: process.env.SOLANA_RPC_URL,
+  ADMIN_PUBLIC_KEY: process.env.ADMIN_PUBLIC_KEY,
+  ADMIN_PRIVATE_KEY: process.env.ADMIN_PRIVATE_KEY,
 };
 
 export const AppConfig = {
