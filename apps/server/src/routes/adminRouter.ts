2f48afd64098de9f4363435d5e174a945a963c18
Mayank <mayanktiwari1212.mt@gmail.com>
2025-05-22 22:55:49 +0530

    fix: Added auth middleware

--- apps/server/src/routes/adminRouter.ts
+++ apps/server/src/routes/adminRouter.ts
@@ -5,7 +5,21 @@ import authMiddleware from "../middlewares/authMiddleware";
 const adminRouter: Router = Router();
 
 // MAKE SURE TO ADD AUTH MIDDLEWARE
-adminRouter.get("/generate-presigned-url", adminController.getPreSignedUrl);
-adminRouter.post("/create-task/:hash", authMiddleware, adminController.createTask);
+adminRouter.get(
+  "/generate-presigned-url",
+  authMiddleware,
+  adminController.getPreSignedUrl
+);
+adminRouter.post(
+  "/create-task/:hash",
+  authMiddleware,
+  adminController.createTask
+);
+adminRouter.get("/get-all-tasks", authMiddleware, adminController.getAllTasks);
+adminRouter.post(
+  "/pause-task/:taskId",
+  authMiddleware,
+  adminController.pauseTask
+);
 
 export default adminRouter;
